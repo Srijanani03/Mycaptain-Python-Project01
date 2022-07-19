8adfe35e3cad5c74575ea691bb073fcf2b137d06
Srijanani03 <109100664+Srijanani03@users.noreply.github.com>
2022-07-19 22:21:32 +0530

    Simple Data Structures Project.py

---
+++ Simple Data Structures Project.py
@@ -0,0 +1,8 @@
+#To find the AREA of a circle when RADIUS is given :
+r=float(input("Input the radius of the circle : "))
+print("The area of the circle with radius " + str(r) + " is : " + str(3.1415*r**2))
+
+#The extension of .py
+filename=input("Input the filename : ")
+f=filename.split(".")
+print("The Extension of the file is : " + (f[-1])+"thon")
